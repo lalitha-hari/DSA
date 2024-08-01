ccee42e6f4468b07962cc3b47c13aca4346f9ed1
lalitha-hari <lalithahari2002@gmail.com>
2024-08-01 21:18:00 +0530

    finding sum in array

---
+++ suminarray.py
@@ -0,0 +1,27 @@
+#Given an array arr of positive integers and another number x. Determine whether two elements exist in arr whose sum is exactly x or not. Return a boolean value true if two elements exist in arr else return false.
+
+def hasArrayTwoCandidates(arr, x): # worst case
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == x:
+                return "true"
+    return "false"
+
+
+
+#o(n) using two pointer theorem
+def hasArrayTwoCandidates(arr, x):
+    arr.sort()  # Sort the array
+    n = len(arr)
+    l = 0
+    h = n - 1
+    
+    while l < h:
+        if x == arr[l] + arr[h]:
+            return 1
+        elif arr[l] + arr[h] > x:
+            h -= 1
+        else:
+            l += 1
+    
+    return 0

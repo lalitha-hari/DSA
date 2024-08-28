9ccebed874def6ee23b8dcf9a6c0e75cf58d9d8f
lalitha-hari <lalithahari2002@gmail.com>
2024-08-28 22:37:02 +0530

    possible_palindrome_subarrays

---
+++ allpalindromesubarray.py
@@ -0,0 +1,23 @@
+def palindromeSubStrs(self, s):
+        new=set()
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                sub=s[i:j]
+                if sub==sub[::-1]:
+                   new.add(sub)
+        return len(new)
+#------------------------------------------------------------------------------
+def palindromeSubStrs(self, s):
+        new=set()
+        
+        def selection(i,j):
+            while i>=0 and j < len(s) and s[i]==s[j]:
+                new.add(s[i:j+1])
+                i-=1
+                j+=1
+         
+        
+        for i in range(len(s)):
+            selection(i,i)
+            selection(i,i+1)
+        return len(new)
